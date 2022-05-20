7d4e111e43a34782e8f8ded83038b93fef9c3deb
Nick Talati <nicktalati@gmail.com>
2022-05-19 23:28:01 -0400

    corrected code based on minimum time step

--- epidemic_simulation.py
+++ epidemic_simulation.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(1234)
+np.set_printoptions(threshold=np.inf)
 
 
 class Simulation:
@@ -17,10 +18,12 @@ class Simulation:
         self.e = e
         self.i = i
         self.r = n - s - e - i
-        self.total_out_of_s_coefficient = None
-        self.total_into_i_coefficient = None
-        self.total_into_r_coefficient = None
+        self.total_out_of_s_coefficient = 0
+        self.total_into_i_coefficient = 0
+        self.total_into_r_coefficient = 0
         self.history = np.asarray([[self.s, self.e, self.i, self.r]])
+        self.total_time = 0
+        self.time_steps = np.array([0])
 
     def update_coefficients(self):
         """
@@ -31,15 +34,20 @@ class Simulation:
         self.total_into_i_coefficient = self.a * self.e
         self.total_into_r_coefficient = self.gamma * self.i
 
-    def update_seir(self, out_of_s, into_i, into_r):
+    def update_seir(self, argmin):
         """
         Updates SEIR categories based on positive numbers produced from sampling the
         Poisson distributions.
         """
-        self.s = self.s - out_of_s
-        self.e = self.e + out_of_s - into_i
-        self.i = self.i + into_i - into_r
-        self.r = self.r + into_r
+        if argmin == 0:
+            self.s = self.s - 1
+            self.e = self.e + 1
+        elif argmin == 1:
+            self.e = self.e - 1
+            self.i = self.i + 1
+        else:
+            self.i = self.i - 1
+            self.r = self.r + 1
 
     def update_history(self):
         """
@@ -49,25 +57,51 @@ class Simulation:
         self.history = np.append(self.history, [[self.s, self.e, self.i, self.r]], axis=0)
 
     def time_step(self):
+        if self.e == 0 and self.i == 0:
+            self.history = np.append(self.history, [[self.s, self.e, self.i, self.r]], axis=0)
+            self.total_time += 1
+            self.time_steps = np.append(self.time_steps, self.total_time)
+            return
         self.update_coefficients()
         """
-        The variables below describe the changes between categories that happen in
-        a given time step. This is simpler than using net change in each category
-        because the values below are all always non-negative; negative values would
-        complicate the calculation of the values from the poisson distribution.
+        This function calculates a random time step for each of the three Poisson processes,
+        selects the minimum one, and then updates the categories accordingly. 
         """
-        out_of_s = min(self.s, np.random.poisson(self.total_out_of_s_coefficient))
-        into_i = min(self.e, np.random.poisson(self.total_into_i_coefficient))
-        into_r = min(self.i, np.random.poisson(self.total_into_r_coefficient))
+        if self.total_out_of_s_coefficient == 0:
+            time_for_out_of_s = np.inf
+        else:
+            time_for_out_of_s = -1 * np.log(np.random.uniform()) / self.total_out_of_s_coefficient
 
-        self.update_seir(out_of_s, into_i, into_r)
+        if self.total_into_i_coefficient == 0:
+            time_for_into_i = np.inf
+        else:
+            time_for_into_i = -1 * np.log(np.random.uniform()) / self.total_into_i_coefficient
+
+        if self.total_into_r_coefficient == 0:
+            time_for_into_r = np.inf
+        else:
+            time_for_into_r = -1 * np.log(np.random.uniform()) / self.total_into_r_coefficient
+
+        time_array = np.asarray([time_for_out_of_s, time_for_into_i, time_for_into_r])
+        self.total_time += min(time_array)
+        self.time_steps = np.append(self.time_steps, self.total_time)
+        argmin = np.argmin(time_array)
+
+        self.update_seir(argmin)
         self.update_history()
 
-    def simulate(self, time_steps):
+    def simulate(self, total_time):
         """
         Simulates SEIR model given specified number of times steps.
         """
-        for _ in range(time_steps):
+        while self.time_steps[-1] < total_time:
+            self.time_step()
+
+    def simulate_till_end(self):
+        """
+        Simulates SEIR model until E and I categories are zero
+        """
+        while self.e != 0 or self.i != 0:
             self.time_step()
 
     def graph(self):
@@ -76,24 +110,59 @@ class Simulation:
         """
         s_data, e_data, i_data, r_data = self.history[:, 0], self.history[:, 1], \
                                          self.history[:, 2], self.history[:, 3]
-        plt.plot(s_data, label='Susceptible subjects')
-        plt.plot(e_data, label='Exposed subjects')
-        plt.plot(i_data, label='Infected subjects')
-        plt.plot(r_data, label='Removed subjects')
+        plt.plot(self.time_steps, s_data, label='Susceptible subjects')
+        plt.plot(self.time_steps, e_data, label='Exposed subjects')
+        plt.plot(self.time_steps, i_data, label='Infected subjects')
+        plt.plot(self.time_steps, r_data, label='Removed subjects')
         plt.legend()
         plt.show()
+
+    def return_unit_time_data(self):
+        """
+        Returns SEIR data indexed by unit time steps
+        """
+        total_time = int(self.time_steps[-1])
+        data = np.asarray([self.history[0]])
+        for i in range(1, total_time + 1):
+            for j in range(len(self.time_steps)):
+                if self.time_steps[j] == i:
+                    data = np.append(data, [self.history[j]], axis=0)
+                    break
+                elif self.time_steps[j] > i:
+                    data = np.append(data, [self.history[j - 1]], axis=0)
+                    break
+        if total_time - self.time_steps[-1] != 0:
+            data = np.append(data, [self.history[-1]], axis=0)
+        return data
+
+
+
+def graph_beta(a, gamma, n, s, i):
+    """
+    Shows how changes in beta parameter affect total number of infected cases.
+    """
+    beta = 0
+    results = []
+    for beta in np.linspace(0, .25, 200):
+        simul = Simulation(beta, a, gamma, n, s=s, i=i)
+        simul.simulate_till_end()
+        final = simul.history[-1][3]
+        results.append(final)
+    plt.plot(np.linspace(0, .25, 200), results)
+    plt.xlabel('beta value')
+    plt.ylabel('Cumulative number of infected at end of simulation')
+    plt.show()
 
 
 if __name__ == '__main__':
     "setting variables"
-    beta = .5   # number of contacts per person per time step
-    a = .2      # parameter controlling latency between exposure and infection
-    gamma = .1  # parameter specifying probability of removal
-    n = 1000    # total population
-    i = 10      # initial number of infected subjects
-    s = n - i   # susceptible subjects
+    beta = .5  # number of contacts per person per time step
+    a = .2  # parameter controlling latency between exposure and infection
+    gamma = .07  # parameter specifying probability of removal
+    n = 1000  # total population
+    i = 10  # initial number of infected subjects
+    s = n - i  # susceptible subjects
 
     sim = Simulation(beta, a, gamma, n, s=s, i=i)
-    sim.simulate(100)
-    print(sim.history)
+    sim.simulate_till_end()
     sim.graph()
