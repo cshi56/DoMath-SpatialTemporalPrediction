a8067aeee547cd91bfdd88d403313c0ebab6c6ab
Nick Talati <nicktalati@gmail.com>
2022-06-17 08:48:40 -0400

    updated this based on our new metric

--- temporal_only/evaluate.py
+++ temporal_only/evaluate.py
@@ -5,52 +5,64 @@ import contextlib
 import matplotlib.pyplot as plt
 
 
-def mse(a, b):
+def mean_squared_error(a, b):
     a = np.asarray(a).flatten()
     b = np.asarray(b).flatten()
     diff = a - b
     return sum(diff ** 2) / len(diff)
 
 
-def evaluate(val_sims, nn_model, input_length, length_of_forecast, stride):
+def evaluate_single_simulation(sim, nn_model, input_length, lst_of_lengths):
+    mses = np.empty((len(lst_of_lengths)))
+
+    sim = np.asarray(sim)
+    n = sum(sim[0])
+    normalized_sim = sim / n
+
+    max_forecast_length = max(lst_of_lengths)
+
+    future_data = np.empty((0, 4))
+
+    current_data = normalized_sim[:input_length]
+    current_data = np.expand_dims(current_data, 0)
+
+    for step in range(max_forecast_length):
+        next_data = np.asarray(nn_model.predict_step(current_data))[0]
+        current_data = current_data[:, 1:]
+        current_data = np.append(current_data, [[next_data]], axis=1)
+        future_data = np.append(future_data, [next_data], axis=0)
+
+    for index in range(len(lst_of_lengths)):
+        forecast_length = lst_of_lengths[index]
+        forecast_index = forecast_length - 1
+        prediction = future_data[forecast_index]
+        actual_data = normalized_sim[input_length + forecast_index]
+        mse = mean_squared_error(prediction, actual_data)
+        mses[index] = mse
+
+    return mses
+
+
+def evaluate(val_sims, nn_model, input_length, seq_of_lengths):
     ret = OrderedDict()
-    mses = []
-    for seir_data in val_sims:
-        n = sum(seir_data[0])
-        seir_data = seir_data / n
-        sim_length = len(seir_data)
+    mses = np.empty((0, len(seq_of_lengths)))
+    for sim in val_sims:
+        mses_to_add = evaluate_single_simulation(sim, nn_model, input_length, seq_of_lengths)
+        mses = np.append(mses, [mses_to_add], axis=0)
 
-        for t in range(0, sim_length - input_length - length_of_forecast, stride):
-            initial_data = seir_data[t:t + input_length]
-            initial_data = np.expand_dims(initial_data, 0)
-            y_hat = []
-
-            for h in range(length_of_forecast):
-                next_data = np.asarray(nn_model.predict_step(initial_data))[0]
-                y_hat.append(next_data)
-                initial_data = initial_data[:, 1:]
-                initial_data = np.append(initial_data, [[next_data]], axis=1)
-
-            y_hat = np.asarray(y_hat)
-            y_actual = seir_data[t + input_length:t + input_length + length_of_forecast]
-            mean_se = mse(y_actual, y_hat)
-            mses.append(mean_se)
-
-    mses = np.asarray(mses)
-    mses_sorted = np.sort(mses)
-    average_mse = mses.mean()
-    std = mses.std()
+    average_mses = np.mean(mses, axis=0)
+    stds = np.std(mses, axis=0)
 
     nn_model.summary()
     ret['Number of validation simulations'] = len(val_sims)
-    ret['Stride between inputs'] = stride
     ret['Length of inputs'] = input_length
-    ret['Total evaluation distance'] = length_of_forecast
-    ret['Average mean squared error'] = average_mse
-    ret['Standard deviation of mean squared error'] = std
-    ret['Sorted MSE values'] = mses_sorted
+    ret['Evaluation distances'] = seq_of_lengths
+    ret['Average mean squared error for each distance'] = average_mses
+    ret['Standard deviation of mean squared error'] = stds
 
     [print(key, ':', value) for key, value in ret.items()]
+    print('MSE values :')
+    print(mses)
     print('\n')
 
 
@@ -60,9 +72,8 @@ if __name__ == '__main__':
     VAL_NUM = 50
     TEST_NUM = 50
     EVALUATION_DIST = 50
-    STRIDE = 50
 
-    MODEL_PATH = 'models/m5'
+    MODEL_PATH = 'models/m6'
     PRIOR_STEPS = 50
     FUTURE_STEPS = 1
 
@@ -70,12 +81,15 @@ if __name__ == '__main__':
     val_file_names = all_sims.files[TRAIN_NUM:TRAIN_NUM + VAL_NUM]
     validation_sims = []
 
+    for elem in validation_sims:
+        print(len(elem))
+
     for file_name in val_file_names:
         validation_sims.append(all_sims[file_name])
 
     model = keras.models.load_model(MODEL_PATH)
 
-    file_path = 'model_evaluations.txt'
+    file_path = 'evaluations.txt'
     with open(file_path, "a") as f:
         with contextlib.redirect_stdout(f):
-            evaluate(validation_sims, model, PRIOR_STEPS, EVALUATION_DIST, STRIDE)
+            evaluate(validation_sims, model, PRIOR_STEPS, [1, 5, 20, 50])
